b2b80c3c56b9aab59ca8b2ff9196127c481e649d
pravinkumarravi <30704490+pravinkumarravi@users.noreply.github.com>
2022-10-21 13:38:24 +0530

    Update netsuite_connector_example.dart

--- example/netsuite_connector_example.dart
+++ example/netsuite_connector_example.dart
@@ -2,13 +2,10 @@ import 'package:netsuite_connector/netsuite_connector.dart';
 
 void main() async {
   const Map<String, String> oauthCredentials = {
-    'consumer_key':
-        'your consumer_key',https://github.com/pravinkumarravi/netsuite_connector/blob/main/example/netsuite_connector_example.dart
-    'consumer_secret':
-        'your consumer_secret',
+    'consumer_key': 'your consumer_key',
+    'consumer_secret': 'your consumer_secret',
     'token': 'your token',
-    'token_secret':
-        'your token_secret',
+    'token_secret': 'your token_secret',
   };
 
   Uri baseUri = Uri.parse(
